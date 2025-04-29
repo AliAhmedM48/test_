1f446d17a22148875b4dd552e4af07d0bc43d4b9
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2025-04-29 10:25:15 +0300

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './sections/hero/hero.component';
+import { AboutUsComponent } from './sections/about-us/about-us.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
+import { ContactUsComponent } from './sections/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeroComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactUsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'JSB_FE_filteration';
